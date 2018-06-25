ad04d479769b0e5f835df667a18b50cac50c43c6
李鑫10 lixin10 () <lixin10@tuniu-inc.com>
2018-06-25 17:18:23 +0800

    Initial commit

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hit.demo;
+import org.testng.annotations.Test;
+public class HelloWorld {
+    @Test
+    public static void main(String[] args){
+        System.out.println("hello levi");
+
+    }
+}
---
+++ src/com/hit/demo/ceshiUser.java
@@ -0,0 +1,8 @@
+package com.hit.demo;
+
+/**
+ * Created by lixin10 on 2018/6/25.
+ */
+public class ceshiUser {
+
+}
